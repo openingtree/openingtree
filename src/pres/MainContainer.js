ed31dcb4d7f19eccfc4b724569c09efef135f3ad
vjuneja <vanuj85@yahoo.co.in>
2020-02-09 22:23:30 -0800

    moved functions to its own file

--- src/pres/MainContainer.js
+++ src/pres/MainContainer.js
@@ -3,18 +3,18 @@ import Chess from 'chess.js'
 import Chessground from 'react-chessground'
 import 'react-chessground/dist/styles/chessground.css'
 import {openingGraph} from '../app/OpeningGraph'
-import PGNLoader from './PGNLoader'
-import SettingsView from './Settings'
 import Navigator from './Navigator'
 import GlobalHeader from './GlobalHeader'
 import {Container, Row, Col} from 'reactstrap'
 import ControlsContainer from './ControlsContainer'
+import {addStateManagement} from './StateManagement'
 
 export default class MainContainer extends React.Component {
   
   constructor(props){
     super(props)
     this.chess = new Chess()
+    addStateManagement(this)
     this.state = {
         fen: this.chess.fen(),
         lastMove: null,
@@ -32,106 +32,6 @@ export default class MainContainer extends React.Component {
 
  
 
-  turnColor() {
-    return this.fullTurnName(this.chess.turn())
-  }
-
-  fullTurnName(shortName) {
-    return shortName === 'w' ? 'white' : 'black'
-  }
-
-  playerColor() {
-    return this.state.settings.playerColor
-  }
-
-  brushes() {
-    if(this.playerColor() === this.turnColor()) {
-      return this.forBrushes
-    }
-    return this.againstBrushes
-  }
-
-  calcMovable() {
-    const dests = {}
-    this.chess.SQUARES.forEach(s => {
-      const ms = this.chess.moves({square: s, verbose: true})
-      if (ms.length) dests[s] = ms.map(m => m.to)
-    })
-    return {
-      free: false,
-      dests,
-      color: this.turnColor()
-    }
-  }
-
-  orientation() {
-    return this.state.settings.orientation
-  }
-
-  onMove(from, to) {
-    const chess = this.chess
-    let move = chess.move({ from, to, promotion: 'q'})
-    this.setState({ fen: chess.fen(), lastMove: move})
-  }
-
-  navigateTo(fen, previousMove){
-    this.chess = new Chess(fen)
-    this.setState({fen:fen, lastMove:previousMove})
-
-  }
-  updateProcessedGames(n, openingGraph) {
-      this.setState({
-        gamesProcessed: this.state.gamesProcessed+n,
-        openingGraph: openingGraph
-      })
-  }
-  moveToShape(move) {
-    return {
-      orig:move.orig,
-                    dest: move.dest,
-                    brush: this.brushes()[move.level]
-    }
-  }
-
-  autoShapes() {
-    var moves
-    if (this.turnColor() === this.playerColor()) {
-      moves = this.state.openingGraph.movesForFen(this.chess.fen())
-    } else {
-      moves = this.state.openingGraph.movesAgainstFen(this.chess.fen())
-    } 
-    if(moves) {
-      var shapes = moves.map(this.moveToShape.bind(this))
-      return this.fillArray(shapes,  25)
-    }
-    return this.fillArray([], 25) // dummy arrow to clear out existing arrows
-  }
-
-  fillArray(arr, len) {
-    for (var i = arr.length; i < len; i++) {
-      arr.push({'orig':'i'+i, 'dest':'i'+(i+1), brush:this.brushes()[0]});
-    }
-    return arr;
-  }
-  
-  
-
-  reset() {
-    this.chess = new Chess()
-    this.setState({fen: this.chess.fen(), lastMove:{from:'ab1',to:'ab2', san:''}})
-  }
-
-  clear() {
-    this.state.openingGraph.clear()
-    this.state.gamesProcessed = 0
-    this.reset()
-  }
-
-  settingsChange(settings) {
-    this.setState({
-      'settings':settings
-    })
-  }
 
   render() {
     let lastMoveArray = this.state.lastMove ? [this.state.lastMove.from, this.state.lastMove.to] : null
---
+++ src/pres/StateManagement.js
@@ -0,0 +1,121 @@
+import Chess from 'chess.js'
+
+function turnColor() {
+    return fullTurnName(this.chess.turn())
+}
+
+function fullTurnName(shortName) {
+    return shortName === 'w' ? 'white' : 'black'
+}
+
+function playerColor() {
+    return this.state.settings.playerColor
+}
+
+function brushes() {
+    if(this.playerColor() === this.turnColor()) {
+        return this.forBrushes
+    }
+    return this.againstBrushes
+}
+
+function calcMovable() {
+const dests = {}
+    this.chess.SQUARES.forEach(s => {
+        const ms = this.chess.moves({square: s, verbose: true})
+        if (ms.length) dests[s] = ms.map(m => m.to)
+    })
+    return {
+        free: false,
+        dests,
+        color: this.turnColor()
+    }
+}
+
+function orientation() {
+    return this.state.settings.orientation
+}
+
+function onMove(from, to) {
+    const chess = this.chess
+    let move = chess.move({ from, to, promotion: 'q'})
+    this.setState({ fen: chess.fen(), lastMove: move})
+}
+
+function navigateTo(fen, previousMove){
+    this.chess = new Chess(fen)
+    this.setState({fen:fen, lastMove:previousMove})
+
+}
+function updateProcessedGames(n, openingGraph) {
+    this.setState({
+    gamesProcessed: this.state.gamesProcessed+n,
+    openingGraph: openingGraph
+    })
+}
+function moveToShape(move) {
+    return {
+        orig:move.orig,
+                    dest: move.dest,
+                    brush: this.brushes()[move.level]
+    }
+}
+
+function autoShapes() {
+    var moves
+    if (this.turnColor() === this.playerColor()) {
+        moves = this.state.openingGraph.movesForFen(this.chess.fen())
+    } else {
+        moves = this.state.openingGraph.movesAgainstFen(this.chess.fen())
+    } 
+    if(moves) {
+        var shapes = moves.map(this.moveToShape.bind(this))
+        return this.fillArray(shapes,  25)
+    }
+    return this.fillArray([], 25) // dummy arrow to clear out existing arrows
+}
+
+function fillArray(arr, len) {
+    for (var i = arr.length; i < len; i++) {
+        arr.push({'orig':'i'+i, 'dest':'i'+(i+1), brush:this.brushes()[0]});
+    }
+    return arr;
+}
+
+
+
+function reset() {
+    this.chess = new Chess()
+    this.setState({fen: this.chess.fen(), lastMove:{from:'ab1',to:'ab2', san:''}})
+}
+
+function clear() {
+    this.state.openingGraph.clear()
+    this.state.gamesProcessed = 0
+    this.reset()
+}
+
+function settingsChange(settings) {
+    this.setState({
+        'settings':settings
+    })
+}
+
+function addStateManagement(obj){
+    obj.orientation  = orientation
+    obj.turnColor = turnColor
+    obj.calcMovable = calcMovable
+    obj.onMove = onMove
+    obj.autoShapes = autoShapes
+    obj.updateProcessedGames = updateProcessedGames
+    obj.settingsChange = settingsChange
+    obj.reset = reset
+    obj.clear = clear
+    obj.navigateTo = navigateTo
+    obj.playerColor = playerColor
+    obj.fillArray = fillArray
+    obj.brushes = brushes
+    obj.moveToShape = moveToShape
+}
+
+export {addStateManagement}
