fb8f789c4ce2afbef53f19aac3b754a3b1a2f44f
vjuneja <vanuj85@yahoo.co.in>
2020-02-10 08:52:29 -0800

    nav icons

--- src/pres/ControlsContainer.js
+++ src/pres/ControlsContainer.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import PGNLoader from './PGNLoader'
 import SettingsView from './Settings'
+import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
+import classnames from 'classnames';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faList } from '@fortawesome/free-solid-svg-icons'
 
 export default class ControlsContainer extends React.Component {
     constructor(props){
@@ -8,15 +12,43 @@ export default class ControlsContainer extends React.Component {
         this.state = {
             lastMove: null,
             playerName:'',
+            activeTab:'1',
             settings:{
               orientation:'white',
               playerColor:'white'
             }
           }
       }
+    toggle(tab) {
+        if(this.state.activeTab !== tab) {
+            this.setState({activeTab:tab})
+        }
+    }
 
     render(){
         return <div>
+            <Nav tabs>
+        <NavItem>
+          <NavLink
+            className={classnames({ active: this.state.activeTab === '1' })}
+            onClick={() => { this.toggle('1'); }}
+          >
+            <FontAwesomeIcon icon={faUser} />
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink
+            className={classnames({ active: this.state.activeTab === '2' })}
+            onClick={() => { this.toggle('2'); }}
+          >
+            <FontAwesomeIcon icon={faList} />
+          </NavLink>
+        </NavItem>
+      </Nav>
+      <TabContent activeTab={this.state.activeTab}>
+        <TabPane tabId="1">
+          <Row>
+            <Col sm="12">
             <div>Number of games processed: {this.props.gamesProcessed}</div>
             <PGNLoader notify = {this.props.updateProcessedGames}/>
             <SettingsView onChange = {this.props.settingsChange}/>
@@ -24,6 +56,27 @@ export default class ControlsContainer extends React.Component {
                 <button onClick = {this.props.reset}>Reset</button>
                 <button onClick = {this.props.clear}>Clear</button>
             </div>
+            </Col></Row>
+            </TabPane>
+        <TabPane tabId="2">
+          <Row>
+            <Col sm="6">
+              <Card body>
+                <CardTitle>Special Title Treatment</CardTitle>
+                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                <Button>Go somewhere</Button>
+              </Card>
+            </Col>
+            <Col sm="6">
+              <Card body>
+                <CardTitle>Special Title Treatment</CardTitle>
+                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                <Button>Go somewhere</Button>
+              </Card>
+            </Col>
+          </Row>
+        </TabPane>
+      </TabContent>
         </div>
     }
 }
