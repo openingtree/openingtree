984b5db44f6efaa16ebd4d48e764cfcd91af7c78
vjuneja <vanuj85@yahoo.co.in>
2020-02-17 12:21:12 -0800

    progress bar

--- src/pres/MovesList.js
+++ src/pres/MovesList.js
@@ -11,22 +11,24 @@ export default class MovesList extends React.Component {
         return <Table>  
             <TableHead>            
             <TableRow>
-                <TableCell>Move</TableCell>
-                <TableCell>Games</TableCell>
+                <TableCell size="small" className="smallCol">Move</TableCell>
+                <TableCell size="small" className="smallCol">Games</TableCell>
                 <TableCell>Results</TableCell>
             </TableRow></TableHead>  
             <TableBody>
         {
             (this.props.movesToShow)? this.props.movesToShow.map(move => 
                 <TableRow>
-                    <TableCell>{move.san}</TableCell>
-                    <TableCell>{move.count}</TableCell>
+                    <TableCell size="small" className="smallCol">{move.san}</TableCell>
+                    <TableCell size="small" className="smallCol">{move.count}</TableCell>
                     <TableCell>
+                        <div className="border borderRadius">
                         <Progress multi>
-                            <Progress bar className="white" color="success" value={`${move.whiteWins/move.count*100}`} />
-                            <Progress bar className="gray" color="info" value={`${move.draws/move.count*100}`} />
-                            <Progress bar className="black" color="danger" value={`${move.blackWins/move.count*100}`} />
+                            <Progress bar className="whiteMove" value={`${move.whiteWins/move.count*100}`} />
+                            <Progress bar className="grayMove" value={`${move.draws/move.count*100}`} />
+                            <Progress bar className="blackMove" value={`${move.blackWins/move.count*100}`} />
                         </Progress>
+                        </div>
                     </TableCell>
                 </TableRow>
             ):""
--- src/pres/Navigator.js
+++ src/pres/Navigator.js
@@ -63,7 +63,7 @@ export default class Navigator extends React.Component {
             this.openingManager.pgnListSoFar()? 
             (this.openingManager.pgnListSoFar().map((move, index)=>
                 <Row onClick={this.moveTo(index).bind(this)} className={`navCol ${this.openingManager.currentMove() === index? 'selectedMove':''}`}>
-                        <Col sm="12" className = "border">{`${move.moveNumber}.${move.whitePly} ${move.blackPly}`}</Col>
+                    <Col sm="12" className = "navMove border">{`${move.moveNumber}.${move.whitePly} ${move.blackPly}`}</Col>
                 </Row>))
             :''}
         </Container>
