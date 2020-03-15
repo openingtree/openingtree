f06f486d8799801e58a6ecee4a3c9940e556e3b0
vjuneja <vanuj85@yahoo.co.in>
2020-03-15 15:39:18 -0700

    recent games first

--- src/app/ChessComIterator.js
+++ src/app/ChessComIterator.js
@@ -1,5 +1,6 @@
 import ChessWebAPI from 'chess-web-api'
 import { parse }  from './PGNParser'
+import request from 'request'
 
 export default class ChessComIterator {
 
@@ -12,15 +13,31 @@ export default class ChessComIterator {
             ready(archiveResponse.body.games.filter(game=>game.rules==="chess").map(game=>parse(game.pgn)[0]))
         }
 
-        let fetchAllGames = function(response) {
-            response.body.archives.forEach((archiveUrl)=>{
+        let fetchAllGames = function(responseBody) {
+            responseBody.archives.reverse().forEach((archiveUrl)=>{
                 let components=archiveUrl.split('/')
                 let year=components[components.length-2]
                 let month=components[components.length-1]
                 chessAPI.dispatch(chessAPI.getPlayerCompleteMonthlyArchives, parseGames, [playerName, year, month]);
             })
         }
-        
-        chessAPI.dispatch(chessAPI.getPlayerMonthlyArchives, fetchAllGames, [playerName]);
+        request(`https://api.chess.com/pub/player/${playerName}/games/archives`, function (error, response, body) {
+            if(error) {
+                showError('Could not connect to chess.com')
+            } else if(response.statusCode === 404) {
+                showError('Could not find chess.com user '+playerName)
+            } else if (response.statusCode !== 200) {
+                showError('Could not load games for chess.com user '+playerName)
+            } else {
+                if(response.body) {
+                    try{
+                        let jsonBody = JSON.parse(response.body)
+                        fetchAllGames(jsonBody)
+                    }catch(e) {
+                        showError('Could not find games for chess.com user '+playerName)
+                    }
+                }
+            }
+        });
     }
 }
