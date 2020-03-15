4ff9caa84d217ef46919680372078b9eabde16f2
vjuneja <vanuj85@yahoo.co.in>
2020-03-14 19:48:58 -0700

    simplified settings layout

--- src/pres/Settings.js
+++ src/pres/Settings.js
@@ -20,14 +20,25 @@ export default class SettingsView extends React.Component {
     }
     render() {
         return <div>
-                <Container><Row><Col xs="6">
-        <Button className="settingButton" onClick = {this.toggle('orientation')} color=""><h3><FontAwesomeIcon icon={faRetweet} /></h3> Flip board</Button>
-        </Col><Col xs="6"><Button className="settingButton" onClick = {this.toggle('playerColor')} color=""><h3><FontAwesomeIcon icon={faRandom} /></h3> Player color</Button>
-        </Col></Row><Row><Col xs="6">
-        <Button className="settingButton" onClick = {this.props.clear} color=""><h3><FontAwesomeIcon icon={faTrashAlt} /></h3> Clear games</Button>
-        </Col><Col xs="6"><Button className="settingButton" onClick = {this.props.reset} color=""><h3><FontAwesomeIcon icon={faFastBackward} /></h3> Starting position</Button>
-        </Col><Col xs="6"><Button className="settingButton" onClick = {this.analyse.bind(this)} color=""><h3><FontAwesomeIcon icon={faDesktop} /></h3> Computer analysis</Button>
-        </Col></Row></Container>
+            <Container>
+                <Row sm="12">
+                    <Col sm="6">
+                        <Button className="settingButton" onClick = {this.toggle('orientation')} color=""><h3><FontAwesomeIcon icon={faRetweet} /></h3> Flip board</Button>
+                    </Col>
+                    <Col sm="6">
+                        <Button className="settingButton" onClick = {this.toggle('playerColor')} color=""><h3><FontAwesomeIcon icon={faRandom} /></h3> Player color</Button>
+                    </Col>
+                    <Col sm="6">
+                        <Button className="settingButton" onClick = {this.props.clear} color=""><h3><FontAwesomeIcon icon={faTrashAlt} /></h3> Clear games</Button>
+                    </Col>
+                    <Col sm="6">
+                        <Button className="settingButton" onClick = {this.props.reset} color=""><h3><FontAwesomeIcon icon={faFastBackward} /></h3> Starting position</Button>
+                    </Col>
+                    <Col sm="6">
+                        <Button className="settingButton" onClick = {this.analyse.bind(this)} color=""><h3><FontAwesomeIcon icon={faDesktop} /></h3> Computer analysis</Button>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     }
 }
