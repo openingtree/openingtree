377cef0741e976dbacc3ba7ab6cbf9ed3db08274
vjuneja <vanuj85@yahoo.co.in>
2020-03-18 00:44:27 -0700

    upgrading projects

---
+++ build/precache-manifest.23d669b7f56b33e74274521849e806df.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0644b8e864d764b7fe8db72677cb56da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37d775db19969cd2aaee",
+    "url": "/static/css/2.3631276c.chunk.css"
+  },
+  {
+    "revision": "37d775db19969cd2aaee",
+    "url": "/static/js/2.e86ec353.chunk.js"
+  },
+  {
+    "revision": "7f9b049a334273fddac3ac5fe6bbe885",
+    "url": "/static/js/2.e86ec353.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a398c01bcce4ea809bf",
+    "url": "/static/js/main.eff6359e.chunk.js"
+  },
+  {
+    "revision": "7d09ecb725a54db5689f",
+    "url": "/static/js/runtime-main.fdfab42b.js"
+  },
+  {
+    "revision": "8a6bd56a8d71b44673a36c25e1ca66aa",
+    "url": "/static/media/bB.8a6bd56a.svg"
+  },
+  {
+    "revision": "47af274b8a88eec771681b9ac53dc6d2",
+    "url": "/static/media/bK.47af274b.svg"
+  },
+  {
+    "revision": "0026f14668177f7dd0d4bab167b1a1cf",
+    "url": "/static/media/bN.0026f146.svg"
+  },
+  {
+    "revision": "344e161b311ce904cc0e3be7d7d61090",
+    "url": "/static/media/bP.344e161b.svg"
+  },
+  {
+    "revision": "aed026d0021b7ebfc2164946c0136010",
+    "url": "/static/media/bQ.aed026d0.svg"
+  },
+  {
+    "revision": "027332a07c4f25930a0e527a85965aa4",
+    "url": "/static/media/bR.027332a0.svg"
+  },
+  {
+    "revision": "970550187aa320d378080ae003b365c1",
+    "url": "/static/media/brown.97055018.svg"
+  },
+  {
+    "revision": "e0eb761a13389f391cf3e6f643597644",
+    "url": "/static/media/wB.e0eb761a.svg"
+  },
+  {
+    "revision": "bb430c603fae05171bfcc6760efda928",
+    "url": "/static/media/wK.bb430c60.svg"
+  },
+  {
+    "revision": "f9c6e522d506c7c3a555e412d34630df",
+    "url": "/static/media/wN.f9c6e522.svg"
+  },
+  {
+    "revision": "4ff1ce7c9541a77b98582a3329e920ed",
+    "url": "/static/media/wP.4ff1ce7c.svg"
+  },
+  {
+    "revision": "4e4432a6b0dfce093177db46dc9bff48",
+    "url": "/static/media/wQ.4e4432a6.svg"
+  },
+  {
+    "revision": "573501233a407695e7b5d5b8e6efc7aa",
+    "url": "/static/media/wR.57350123.svg"
+  }
+]);
