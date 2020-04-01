e3fabc827e6f90bdf8b90abefadff055b2bdab57
vjuneja <vanuj85@yahoo.co.in>
2020-03-31 23:44:00 -0700

    fixed controls covering rank labels on mobile

--- src/pres/MainContainer.js
+++ src/pres/MainContainer.js
@@ -72,7 +72,7 @@ export default class MainContainer extends React.Component {
       }}
       style={{ margin: 'auto' }}
     />
-    </Col><Col lg="4"><ControlsContainer fen={this.state.fen}
+    </Col><Col lg="4" className="paddingTop"><ControlsContainer fen={this.state.fen}
                 gamesProcessed={this.state.gamesProcessed} 
                 updateProcessedGames={this.updateProcessedGames.bind(this)}
                 settingsChange={this.settingsChange.bind(this)}
