fd0c97c7f3e88e007a3c0ed7147ebfa6c241b709
vjuneja <vanuj85@yahoo.co.in>
2020-04-16 19:24:24 -0700

    actions component

--- src/pres/loader/PGNLoader.js
+++ src/pres/loader/PGNLoader.js
@@ -1,16 +1,11 @@
 import React from 'react'
-import PGNReader from '../../app/PGNReader'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Button as MaterialUIButton } from '@material-ui/core'
-import { faList} from '@fortawesome/free-solid-svg-icons'
 import { createSubObjectWithProperties, getTimeframeSteps } from '../../app/util'
 import * as Constants from '../../app/Constants'
 import { trackEvent } from '../../app/Analytics'
-import GetApp from '@material-ui/icons/GetApp';
-import Equalizer from '@material-ui/icons/Equalizer';
 import Source from './Source'
 import User from './User'
 import Filters from './Filters'
+import Actions from './Actions'
 
 export default class PGNLoader extends React.Component {
 
@@ -38,18 +33,6 @@ export default class PGNLoader extends React.Component {
         this.state[Constants.FILTER_NAME_ELO_RANGE] = [0, Constants.MAX_ELO_RATING]
     }
 
-    unload = () => {
-        if (this.pgnReader) {
-            this.pgnReader.stopDownloading()
-        }
-    }
-    componentDidMount() {
-        window.addEventListener("beforeunload", this.unload);
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener("beforeunload", this.unload);
-    }
 
     advancedFilters() {
         return createSubObjectWithProperties(this.state,
@@ -62,6 +45,11 @@ export default class PGNLoader extends React.Component {
     }
 
 
+    handleExpansionChange(panel) {
+        return (event, newExpanded) => {
+            this.setState({ expandedPanel: newExpanded ? panel : false });
+        };
+    }
 
     playerNameChange(playerName) {
         this.setState({
@@ -71,54 +59,7 @@ export default class PGNLoader extends React.Component {
         trackEvent(Constants.EVENT_CATEGORY_PGN_LOADER, "PlayerNameSaved")
     }
 
-    readPgn(shouldDownloadToFile) {
-        this.pgnReader = new PGNReader()
-        this.pgnReader.fetchPGNFromSite(this.state.playerName,
-            this.state.playerColor,
-            this.state.site,
-            shouldDownloadToFile,
-            this.advancedFilters(this.state),
-            this.props.notify,
-            this.props.showError,
-            this.stopDownloading.bind(this))
-    }
-    handleExpansionChange(panel) {
-        return (event, newExpanded) => {
-            this.setState({ expandedPanel: newExpanded ? panel : false });
-        };
-    }
 
-    download() {
-        if (!this.state.playerName) {
-            this.props.showError("Please enter a username")
-            return
-        }
-        this.readPgn(true)
-        trackEvent(Constants.EVENT_CATEGORY_PGN_LOADER, "Download", this.state.site, this.state.playerColor === Constants.PLAYER_COLOR_WHITE ? 1 : 0)
-
-    }
-
-    load() {
-        if (!this.state.playerName) {
-            this.props.showError("Please enter a username")
-            return
-        }
-        this.props.clear()
-        this.setState({ isGamesSubsectionOpen: true })
-        // set the player name and color in the global state
-        this.props.onChange("playerName", this.state.playerName)
-        this.props.onChange("playerColor", this.state.playerColor)
-        this.readPgn(false)
-        this.props.setDownloading(true)
-        trackEvent(Constants.EVENT_CATEGORY_PGN_LOADER, "Load", this.state.site, this.state.playerColor === Constants.PLAYER_COLOR_WHITE ? 1 : 0)
-    }
-    stopDownloading() {
-        this.props.setDownloading(false)
-    }
-    stopDownloadingAction() {
-        this.stopDownloading()
-        trackEvent(Constants.EVENT_CATEGORY_PGN_LOADER, "StopDownloading", this.state.site)
-    }
     siteChange(event) {
         this.setState({ site: event.target.value, expandedPanel:'user'})
         trackEvent(Constants.EVENT_CATEGORY_PGN_LOADER, "ChangeSite", this.state.site)
@@ -142,38 +83,12 @@ export default class PGNLoader extends React.Component {
                 site={this.state.site} advancedFilters={this.advancedFilters()}
                 timeframeSteps={this.timeframeSteps} filtersChange={this.filtersChange.bind(this)}/>
             </div>
-            <div style={this.state.site===''?{display:`none`}:{}}>
-            <div className="pgnloadersection"><MaterialUIButton
-                onClick={this.load.bind(this)}
-                variant="contained"
-                color="primary"
-                startIcon={<Equalizer />}
-                className="mainButton" disableElevation
-            >
-                Analyze games
-            </MaterialUIButton></div>
-            <div className="pgnloadersection"><MaterialUIButton
-                onClick={this.download.bind(this)}
-                variant="contained"
-                color="default"
-                startIcon={<GetApp />}
-                className="mainButton" disableElevation
-            >
-                Export as PGN
-            </MaterialUIButton></div>
-            {
-                this.state.isGamesSubsectionOpen ?
-                    <div>
-                        <div className="pgnloadersection">
-                            {`Games Loaded: ${this.props.gamesProcessed} `}{this.props.isDownloading ? <span className="stopDownloading">[<span className="linkStyle" onClick={this.stopDownloadingAction.bind(this)}><img alt="loading spinner" src="./spinner.gif" height="15" />stop</span>]</span> : ""}
-                        </div>
-                        <div onClick={() => this.props.switchToMovesTab()} className="navLinkButton pgnloadersection">
-                            <FontAwesomeIcon icon={faList} /> View Moves>>
-                    </div>
-                    </div>
-                    : ""
-            }
-        </div>
+            <Actions expandedPanel={this.state.expandedPanel} playerColor={this.state.playerColor}
+                playerName={this.state.playerName} site={this.state.site} advancedFilters={this.advancedFilters()}
+                notify={this.props.notify} showError={this.props.showError} onChange={this.props.onChange}
+                setDownloading={this.props.setDownloading} clear={this.props.clear} isDownloading={this.props.isDownloading}
+                switchToMovesTab={this.props.switchToMovesTab} gamesProcessed={this.props.gamesProcessed}/>
+
         </div>
     }
 
