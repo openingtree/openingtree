14275c7fa6cd71122168474964f28625f0ce3c80
vjuneja <vanuj85@yahoo.co.in>
2020-04-18 00:17:58 -0700

    pgn file summary

--- src/pres/loader/Dropzone.js
+++ src/pres/loader/Dropzone.js
@@ -1,24 +1,13 @@
 import React from 'react'
 import {DropzoneArea} from 'material-ui-dropzone'
 
-export default class FileUpload extends React.Component{
-    constructor(props){
-      super(props);
-      this.state = {
-        files: []
-      };
-    }
-    handleChange(files){
-      this.setState({
-        files: files
-      });
-    }
+export default class Dropzone extends React.Component{
     render(){
       return (
         <DropzoneArea 
         acceptedFiles={[]}
         dropzoneText="Drag and drop up to 10 pgn files here or click to select"
-          onChange={this.handleChange.bind(this)}
+          onChange={this.props.filesChange}
           dropzoneParagraphClass="dropzonetext"
           showFileNames={true}
           useChipsForPreview={true}
