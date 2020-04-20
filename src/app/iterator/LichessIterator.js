aecf9196ded48c6df358d5d25f8f4a53bc343006
vjuneja <vanuj85@yahoo.co.in>
2020-04-20 15:20:27 -0700

    base url iterator

--- src/app/iterator/LichessIterator.js
+++ src/app/iterator/LichessIterator.js
@@ -3,6 +3,7 @@ import { parse }  from '../PGNParser'
 import {getTimeControlsArray, getTimeframeSteps, getSelectedTimeFrameData, isOpponentEloInSelectedRange} from '../util'
 import * as Constants from '../Constants'
 import {trackEvent} from '../Analytics'
+import BaseUrlIterator from './BaseUrlIterator'
 
 export default class LichessIterator {
 
@@ -18,52 +19,41 @@ export default class LichessIterator {
         let selectedTimeControls = getTimeControlsArray(Constants.SITE_LICHESS, advancedFilters, true)
         let perfFilter = selectedTimeControls.length === 0 || selectedTimeControls.length === 6?
                 "" : `&perfType=${selectedTimeControls.join(",")}`
-        let requestObject = request.get(
-            lichessBaseURL+playerNameFilter+colorFilter+ratedFilter+perfFilter+timeSinceFilter+timeUntilFilter, 
-            { json: false }).on('error', (error)=> {
+        new BaseUrlIterator(lichessBaseURL+playerNameFilter+colorFilter+ratedFilter+perfFilter+timeSinceFilter+timeUntilFilter,
+            (responseCode)=>{
+                if(responseCode === 404) {
+                    showError('could not find lichess user ' + playerName)
+                } else if (responseCode !== 200) {
+                    showError('could not load games of lichess user ' + playerName)
+                }
+            }, (error)=> {
                 showError('failed to connect to lichess.org')
                 ready([], false)
-        }).on('response',(response)=>{
-            if(response.statusCode === 404) {
-                showError('could not find lichess user ' + playerName)
-            } else if (response.statusCode !== 200) {
-                showError('could not load games of lichess user ' + playerName)
-            }
-        }).on('data', (data) => {
-            let newBody = remainingBody + data.toString();
-            let lastValidPGN = newBody.lastIndexOf("\n\n\n")
-            let body = newBody.slice(0, lastValidPGN).trim()
-
-            remainingBody = newBody.slice(lastValidPGN).trim()
-            let pgnsArray = body.split("\n\n\n")
-
-            let parsedPGNs = pgnsArray.map((pgnString)=> {
-                try {
-                    return parse(pgnString)[0]
-                } catch (e) {
-                    console.log("failed to parse pgn", pgnString)
-                    console.log(e)
-                    trackEvent(Constants.EVENT_CATEGORY_ERROR, "parseFailedLichess", playerName)
-                    return null
-                }
+            }, (pgnStringArray) => {
+                let parsedPGNs = pgnStringArray.map((pgnString)=> {
+                    try {
+                        return parse(pgnString)[0]
+                    } catch (e) {
+                        console.log("failed to parse pgn", pgnString)
+                        console.log(e)
+                        trackEvent(Constants.EVENT_CATEGORY_ERROR, "parseFailedLichess", playerName)
+                        return null
+                    }
+                })
+    
+                let continueProcessing = ready(parsedPGNs.filter((pgn)=>{
+                    if(!pgn || pgn.headers.Variant !== "Standard") {
+                        return false
+                    }
+                    let opponentElo = playerColor === Constants.PLAYER_COLOR_WHITE?pgn.headers.BlackElo:pgn.headers.WhiteElo
+                    if(!isOpponentEloInSelectedRange(opponentElo, advancedFilters[Constants.FILTER_NAME_ELO_RANGE])) {
+                        return false
+                    }
+                    return true
+                }), true)
+                return continueProcessing
+            }, ()=>{
+                ready([], false)
             })
-
-            let continueProcessing = ready(parsedPGNs.filter((pgn)=>{
-                if(!pgn || pgn.headers.Variant !== "Standard") {
-                    return false
-                }
-                let opponentElo = playerColor === Constants.PLAYER_COLOR_WHITE?pgn.headers.BlackElo:pgn.headers.WhiteElo
-                if(!isOpponentEloInSelectedRange(opponentElo, advancedFilters[Constants.FILTER_NAME_ELO_RANGE])) {
-                    return false
-                }
-                return true
-            }), true)
-
-            if(!continueProcessing) {
-                requestObject.abort()
-            }
-        }).on('end', () => {
-            ready([], false)
-        })
     }
 }
