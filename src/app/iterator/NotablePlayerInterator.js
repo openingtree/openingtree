a26ac0e07153be2380610906f378b7bd1da6e0bf
vjuneja <vanuj85@yahoo.co.in>
2020-04-20 16:41:34 -0700

    spelling error

---
+++ src/app/iterator/NotablePlayerInterator.js
@@ -0,0 +1,46 @@
+import request from 'request'
+import { parse }  from '../PGNParser'
+import {getTimeControlsArray, getTimeframeSteps, getSelectedTimeFrameData, isOpponentEloInSelectedRange} from '../util'
+import * as Constants from '../Constants'
+import {trackEvent} from '../Analytics'
+import BaseUrlIterator from './BaseUrlIterator'
+
+export default class NotablePlayerIterator {
+
+    constructor(selectedPlayer, playerColor, advancedFilters, ready, showError) {
+        let lowerCaseAliases = selectedPlayer.pgnAliases.map(alias=>alias.toLowerCase())
+        new BaseUrlIterator(selectedPlayer.pgnUrl,
+            (responseCode)=>{
+                if (responseCode !== 200) {
+                    showError('could not load url')
+                }
+            }, (error)=> {
+                showError('could not connect to url')
+                ready([], false)
+            }, (pgnStringArray) => {
+                let parsedPGNs = pgnStringArray.map((pgnString)=> {
+                    try {
+                        return parse(pgnString)[0]
+                    } catch (e) {
+                        console.log("failed to parse pgn", pgnString)
+                        console.log(e)
+                        trackEvent(Constants.EVENT_CATEGORY_ERROR, "parseFailedPGNUrl")
+                        return null
+                    }
+                })
+    
+                let continueProcessing = ready(parsedPGNs.filter((game)=>{
+                    if(!game) {
+                        return false
+                    }
+                    if(!lowerCaseAliases.includes(game[playerColor].username.toLowerCase())) {
+                        return false
+                    }
+                    return true
+                }), true)
+                return continueProcessing
+            }, ()=>{
+                ready([], false)
+            })
+    }
+}
