a582fe4ac417c846f1a0fc288777db6b6237808e
vjuneja <vanuj85@yahoo.co.in>
2020-04-21 01:21:39 -0700

    fixed bug where games were not getting flushed

--- src/app/iterator/BaseUrlIterator.js
+++ src/app/iterator/BaseUrlIterator.js
@@ -12,22 +12,33 @@ export default class BaseUrlIterator {
             .on('response',(response)=>{
                 return responseCodeCallback(response.statusCode)
             }).on('data', (data) => {
+                // parser cannot handle \r characters
+                // normalize newlines coverts everything to \n
                 let dataString = normalizeNewLine(data.toString())
                 
+                // some pgn files dont have triple \n separating games
+                // so converting all double \n with triple \n if it happens before a [
+                // this separates games with atleast a triple \n
                 let newBody = (remainingBody + dataString).replace(/\n\n\[/g, `\n\n\n[`);
                 let lastValidPGN = newBody.lastIndexOf(`\n\n\n`)
-                let body = newBody.slice(0, lastValidPGN).trim()
+                let body = newBody.slice(0, lastValidPGN)
 
-                remainingBody = newBody.slice(lastValidPGN).trim()
-                let pgnsArray = body.split(`\n\n\n`)
-                                .map(
-                                    pgnString=> pgnString.replace(/^\s+|\s+$/g, '')//trim string
-                                )
-                if(!dataCallback(pgnsArray)) {
+                remainingBody = newBody.slice(lastValidPGN)
+                if(!this.callDataCallback(body, dataCallback)) {
                     requestObject.abort()
                 }
             }).on('end', () => {
+                this.callDataCallback(remainingBody, dataCallback)
                 endCallback()
             })
+    }
+
+    callDataCallback(body, dataCallback) {
+        let pgnArray = body.split(`\n\n\n`)
+            .map(
+                pgnString=> pgnString.replace(/^\s+|\s+$/g, '')//trim string
+            )
+        return dataCallback(pgnArray)
+
     }
 }
