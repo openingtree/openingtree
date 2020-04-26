2d1e67483dd398e0b99d614399b7a6652febb4db
vjuneja <vanuj85@yahoo.co.in>
2020-04-26 16:20:39 -0700

    single line comments

--- src/app/iterator/IteratorUtils.js
+++ src/app/iterator/IteratorUtils.js
@@ -4,16 +4,19 @@ export function normalizePGN(pgnString) {
     // parser cannot handle \r characters
     // normalize newlines coverts everything to \n
     let dataString = normalizeNewLine(pgnString)
+
+    // Remove Single line commments because parser does not handle them
+    dataString = dataString.replace(/^;.*$/gm, '')
     
     // some pgn files dont have triple \n separating games
     // so converting all double \n with triple \n if it happens before a [
     // this separates games with atleast a triple \n
-    let headersNormalized = dataString.replace(/\n\n+\[/g, `\n\n\n[`);
+    dataString = dataString.replace(/\n\n+\[/g, `\n\n\n[`);
 
     // some pgn files have the result in a separate line instead of 
     // at the end of the moves this moves those results to the end of the moves
     // $1 replaces the first captured group which is the result
-    let resultsNormalized = headersNormalized.replace(/\s*\n+(1\-0|0\-1|1\/2\-1\/2)/g, ` $1`);
+    dataString = dataString.replace(/\s*\n+(1\-0|0\-1|1\/2\-1\/2)/g, ` $1`);
 
-    return resultsNormalized
+    return dataString
 }
