d033c8c91b1ee6d7c3c99a14193d40f79790201a
vjuneja <vanuj85@yahoo.co.in>
2020-06-17 01:52:17 -0700

    remove errors

--- src/edge/lichesslogin/src/worker.js
+++ src/edge/lichesslogin/src/worker.js
@@ -1,36 +1,91 @@
 /* eslint-env worker, browser, commonjs */
-/* globals */ // list injected variables after 'globals'
-const axios = require('axios')
-const ClientOAuth2 = require('client-oAuth2')
+/* globals INJECTED_VARIABLE GREETING helloNamespace */
 
+// `require()`ed modules and our Worker script are bundled into a single script
+//  at build time Alternatively, you can use import statements, e.g.: import
+//  sample from 'lodash.sample'
+const sample = require(`lodash.sample`)
+
+// This is our worker's listener function
 addEventListener(`fetch`, event => {
   event.respondWith(requestHandler(event.request))
 })
 
-const tokenHost = 'https://oauth.lichess.org';
-const authorizePath = '/oauth/authorize';
-const tokenPath = '/oauth';
-
-const redirectUri = `https://lichesslogin.openingtree.workers.dev`;
-
+// Our main function which handles requests
 async function requestHandler(request) {
-  // Do awesome stuff here
+  // First, fetch the original request data
+  const response = await fetch(request)
 
-  const oauth2 = new ClientOAuth2({
-    clientId: "LICHESS_CLIENT_ID",
-    clientSecret: "LICHESS_CLIENT_SECRET",
-    accessTokenUri: tokenHost+tokenPath,
-    authorizationUri: tokenHost+authorizePath,
-    redirectUri: redirectUri,
-    scopes: ['preferences:read']
-  })
-  try {
-    const token = await oauth2.code.getToken(request.url)
-    //const userInfo = await getUserInfo(token.token);
-    return new Response('success',{status:200})//res.send(`<h1>Success!</h1>Your lichess user info: <pre>${JSON.stringify(userInfo.data)}</pre>`);
-  } catch(error) {
-    console.error('Access Token Error', error.message);
-    return new Response("abc+"+error.message,{status:200})
-    //res.status(500).json('Authentication failed');
+  // A random 'hello' from out helper function
+  const hello = await utilityFunction()
+  const greeting = `${hello}, world!`
+
+  // Create a new Headers object for our modified response
+  const modifiedHeaders = new Headers(response.headers)
+
+  // Set our greeting header in the modified response
+  modifiedHeaders.set(`x-worker-greeting`, greeting)
+
+  // Complete configuration for our modified response
+  const modifiedResponseOptions = {
+    status: response.status,
+    statusText: response.statusText,
+    headers: modifiedHeaders,
   }
+
+  // return the original content with our modified response options
+  return new Response(response.body, modifiedResponseOptions)
+}
+
+// A simple helper function for the requestHandler
+async function utilityFunction() {
+  const hellos = [
+    'Hello',
+    'Konnichi wa',
+    'Goedendag',
+    'Bonjour',
+    'Shalom',
+    'Buonguiorno',
+    'Dzien dobry',
+    'Moin moin',
+    'Alo',
+    'Gauden Dag',
+    'Privet',
+    'Hola',
+    'God dag',
+    'Xin chao',
+  ]
+
+  try {
+    if (INJECTED_VARIABLE) {
+      /*
+        Below is EXAMPLE_GREETING found in the .env config file (default: 'Guten
+        Tag') It's passed via webpack.config.js to Webpack, which then injects
+        the greeting upon build
+      */
+      hellos.push(INJECTED_VARIABLE)
+    }
+
+    if (GREETING) {
+      /*
+        Below is the secret variable GREETING found in src/example.metadata.json
+        (default: 'Howdy') The 'secret' is stored in the Cloudflare Worker
+        Secrets Vault and automatically injected when the script is executed.
+      */
+      hellos.push(GREETING)
+    }
+
+    if (helloNamespace) {
+      /*
+        Below is the kv_greeting (default: 'Miten menee') found in our
+        helloNamespace namespace, which we bind to the worker via
+        example.metadata.json. The KV
+      */
+      hellos.push(await helloNamespace.get(`kv_greeting`))
+    }
+  } catch (err) {
+    console.error(err)
+  }
+  // Here, we use the lodash.sample module that we 'required' above
+  return sample(hellos)
 }
