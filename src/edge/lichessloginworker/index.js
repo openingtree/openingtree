21ed36d5ab5031a17936a18394018911b29d57fc
vjuneja <vanuj85@yahoo.co.in>
2020-06-21 21:04:36 -0700

    axios

--- src/edge/lichessloginworker/index.js
+++ src/edge/lichessloginworker/index.js
@@ -1,63 +1,59 @@
-const axios = require('axios');
+const axios = require('axios')
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 
-window={}
+window = {}
 
-const tokenHost = 'https://oauth.lichess.org';
-const authorizePath = '/oauth/authorize';
-const tokenPath = '/oauth';
-
-const redirectUri = `https://lichesslogin.openingtree.workers.dev`;
-var ClientOAuth2 = require('client-oauth2')
+const tokenHost = 'https://oauth.lichess.org'
+const tokenPath = '/oauth'
+const redirectUri = `https://lichessloginworker.openingtree.com/`
 
 /**
  * Respond to the request
  * @param {Request} request
  */
 async function handleRequest(req) {
-
-  const oauth2 = new ClientOAuth2({
-    clientId: LICHESS_CLIENT_ID,
-    clientSecret: LICHESS_CLIENT_SECRET,
-    accessTokenUri: tokenHost+tokenPath,
-    authorizationUri: tokenHost+authorizePath,
-    redirectUri: redirectUri,
-    scopes: ['preferences:read']
-  })/*simpleOAuth.create({
-    client: {
-      id: LICHESS_CLIENT_ID,
-      secret: LICHESS_CLIENT_SECRET,
-    },
-    auth: {
-      tokenHost,
-      tokenPath,
-      authorizePath,
-    },
-  });*/
-
   try {
-    const token = await oauth2.code.getToken(req.url)
+    let params = getUrlVars(req.url)
+    let code = params.code
+
+    const tokenResponse = await getTokenResponse(code)
     //const userInfo = await getUserInfo(token.token);
-    return new Response('success',{status:200})//res.send(`<h1>Success!</h1>Your lichess user info: <pre>${JSON.stringify(userInfo.data)}</pre>`);
-  } catch(error) {
-    console.error('Access Token Error', error.message);
-    return new Response("abc+"+error.message,{status:200})
+    return new Response(JSON.stringify(tokenResponse), { status: 200 }) //res.send(`<h1>Success!</h1>Your lichess user info: <pre>${JSON.stringify(userInfo.data)}</pre>`);
+  } catch (error) {
+    console.error('Access Token Error', error.message)
+    return new Response('avc+' + error.message, { status: 200 })
     //res.status(500).json('Authentication failed');
   }
 }
 function getUrlVars(url) {
-  var vars = {};
-  url.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-      vars[key] = value;
-  });
-  return vars;
+  var vars = {}
+  url.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
+    vars[key] = value
+  })
+  return vars
 }
 
-function getUserInfo(token) {
-  return axios.get('/api/account', {
-    baseURL: 'https://lichess.org/',
-    headers: { 'Authorization': 'Bearer ' + token.access_token }
-  });
+function getTokenResponse(code) {
+  var bodyFormData = new FormData()
+  bodyFormData.set('client_id', LICHESS_CLIENT_ID)
+  bodyFormData.set('grant_type', 'authorization_code')
+  return axios({
+    method: 'post',
+    url: tokenHost + tokenPath,
+    data: bodyFormData,
+    headers: { 'Content-Type': 'multipart/form-data' },
+    adapter: require('axios/lib/adapters/http'),
+  })
+    .then(function(response) {
+      //handle success
+      console.log(response)
+      return { test: 'success' }
+    })
+    .catch(function(error) {
+      //handle error
+      console.log(error)
+      return { test1: JSON.stringify(error) }
+    })
 }
