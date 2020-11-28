2d44da89e23059a45bfab431e476a47042df3985
vjuneja <vanuj85@yahoo.co.in>
2020-11-28 08:01:40 -0800

    FAQ component

--- src/pres/GlobalHeader.js
+++ src/pres/GlobalHeader.js
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faComments, faCaretDown, faQuestionCircle} from '@fortawesome/free-solid-svg-icons'
 import { faDiscord } from '@fortawesome/free-brands-svg-icons'
 import * as Constants from '../app/Constants'
+import Faq from 'react-faq-component'
 
 import {
   Navbar,
@@ -21,7 +22,7 @@ import {
   Button,
   ListGroup,
   ListGroupItem,
-  NavItem, NavLink
+  NavItem, NavLink, ModalBody
 } from 'reactstrap'
 
 const GlobalHeader = (props) => {
@@ -85,24 +86,69 @@ const GlobalHeader = (props) => {
     </UncontrolledDropdown>
     
   }
+
+const data = {
+  rows: [
+      {
+          title: "What is OpeningTree.com?",
+          content: `OpeningTree.com is an open source website that helps you analyze anyone's lichess or chess.com games. You can use it to prepare for your opponents, improve your own game or learn from openings that grandmasters play.`,
+      },
+      {
+        title: "How do I use OpeningTree?",
+        content: `To try it out, you can load Magnus Carlsen's games by choosing, lichess as the source, enter "DrNykterstein" in the user name, choose "white" and click "Analyze games". Hit the stop link when a few hundred games have loaded and click "View moves". You can see the moves that Carlsen played as white in his recent games and his win percentages there. You can dig deeper into various lines. The moves shown for black are the moves that were played by his opponents against him.`,
+      },
+      {
+        title: "What are some of the advanced features of OpeningTree?",
+        content: `You can analyze games of notable grandmasters from Morphy to Nakamura. You can also analyze games from lichess tournaments to see which moves are being played by others. You can also save openingtree files locally and load them fast or load them from pgn files.`,
+      },
+      
+      {
+        title: "Do I need to login to lichess?",
+        content: `You dont need to, but logging in increases the speed of download of games from lichess. So if you plan on processing hundreds of games, it is recommended that you login.`,
+      },
+      {
+        title: "Do I need to keep reloading games everytime I want to analyze the same player? OR What is a .tree file?",
+        content: `If you plan to keep reloading the same player's games over and over again. The better option is to use the "Save .tree file" button after your games have loaded. This saves a pre-processed file of all the games on your local computer. You can then load the file using the "Load .tree file" option as the source and load the same file. It can load thousands of games in a matter of seconds.`,
+      },
+      {
+        title: "Is there a way to filter games by time control, choose only recent games etc.?",
+        content:`Yes. You can click on "Advanced filters" under the "Color and filters" panel of openingtree to see many different filtering options that are available.`,
+      },
+      {
+        title: "What do the triangles under the bars indicate?",
+        content: `Little triangles indicate the openingbook statistics on the moves tab and the player statistics on the openingbook tab.
+        Everything left of the white triangle is a win for white, everything right of the black triangle is a win for black and everything in the middle is a draw.
+        If there is only a black triangle and no white, it means there are no draws.`,
+      },
+      {
+          title: "Whenever i click on a square, openingtree selects one square to the left of it.",
+          content: `This is a know bug with a component that I am using. You can work around it by resizing your browser window, or zooming in and back out. It should resolve the issue.`,
+      },
+
+  ],
+};
+
+const styles = {
+  // bgColor: 'white',
+  rowTitleColor: "black",
+   rowContentColor: 'grey',
+   rowTitleTextSize:'small',
+   rowContentTextSize:'small',
+   rowContentPaddingTop:'1'
+  // arrowColor: "red",
+};
+
+const config = {
+  // animate: true,
+  // arrowIcon: "V",
+  // tabFocus: true
+};
   const getFAQModal = ()=>{
     return <Modal isOpen={isFAQOpen} toggle={toggleFAQModal}>
     <ModalHeader toggle={toggleFAQModal}>Frequently asked questions</ModalHeader>
-    <ListGroup className="acknowledgement">
-    <ListGroupItem tag="a" href="https://lichess.org/api" target="_blank" action>Lichess API</ListGroupItem>
-    <ListGroupItem tag="a" href="https://www.chess.com/club/chess-com-developer-community" target="_blank" action>Chess.com API</ListGroupItem>
-    <ListGroupItem tag="a" href="https://github.com/ruilisi/react-chessground" target="_blank" action>ChessGround</ListGroupItem>
-    <ListGroupItem tag="a" href="https://github.com/jhlywa/chess.js" target="_blank" action>Chess.js</ListGroupItem>
-    <ListGroupItem tag="a" href="https://github.com/niklasf/eco" target="_blank" action>Eco</ListGroupItem>
-    <ListGroupItem tag="a" href="https://github.com/jimmywarting/StreamSaver.js" target="_blank" action>StreamSaver.js</ListGroupItem>
-    <ListGroupItem tag="a" href="https://www.flaticon.com/authors/google/" target="_blank" action>Icons made by google</ListGroupItem>
-    <ListGroupItem tag="a" href="https://www.flaticon.com/" target="_blank" action>FlatIcons.com</ListGroupItem>
-    <ListGroupItem tag="a" href="https://www.freelogodesign.org" target="_blank" action>Free logo design</ListGroupItem>
-    <ListGroupItem tag="a" href="https://github.com/kevinludwig/pgn-parser" target="_blank" action>PGN Parser</ListGroupItem>
-    <ListGroupItem tag="a" href="https://www.npmjs.com/package/material-ui-dropzone" target="_blank" action>Dropzone</ListGroupItem>
-    <ListGroupItem tag="a" href="https://www.pgnmentor.com" target="_blank" action>PGN Mentor</ListGroupItem>
-    <ListGroupItem tag="a" href="https://www.twitch.tv/imrosen" target="_blank" action>IM Eric Rosen</ListGroupItem>
-    </ListGroup>
+    <ModalBody>
+    <Faq data={data} styles={styles} config={config} />
+    </ModalBody>
     <ModalFooter>
       <Button color="secondary" onClick={toggleFAQModal}>Done</Button>
     </ModalFooter>
