96b860feb6f48e57947dbf0af2d3b37519ea270b
Xander Marjoram <xander.marjoram@just-eat.com>
2020-12-18 12:39:54 +0000

    Tidy up imports and render function

--- src/pres/MainContainer.js
+++ src/pres/MainContainer.js
@@ -1,36 +1,46 @@
 import React from 'react'
 import Chessground from 'react-chessground'
 import 'react-chessground/dist/styles/chessground.css'
-import OpeningGraph from '../app/OpeningGraph'
-import Navigator from './Navigator'
-import GlobalHeader from './GlobalHeader'
-import {Container, Row, Col} from 'reactstrap'
-import ControlsContainer from './ControlsContainer'
-import {addStateManagement} from './StateManagement'
-import {Snackbar, TextField} from '@material-ui/core'
-import SnackbarContentWrapper from './SnackbarContentWrapper'
-import * as Constants from '../app/Constants'
-import {  Modal, ModalBody,
-  ModalHeader,
+import {
+  Button,
+  Col,
+  Collapse,
+  Container,
+  Modal,
+  ModalBody,
   ModalFooter,
-  Button,Collapse
+  ModalHeader,
+  Row
 } from 'reactstrap'
-import {chessLogic} from '../app/chess/ChessLogic'
 
-import {FormControlLabel, Checkbox} from '@material-ui/core'
+import {
+  Checkbox,
+  FormControlLabel,
+  Snackbar,
+  TextField
+} from '@material-ui/core'
+
+import * as Constants from '../app/Constants'
+import OpeningGraph from '../app/OpeningGraph'
+import { chessLogic } from '../app/chess/ChessLogic'
 import cookieManager from '../app/CookieManager'
 import UserProfile, { USER_PROFILE_NEW_USER } from '../app/UserProfile'
 import {trackEvent} from '../app/Analytics'
 
+import Navigator from './Navigator'
+import GlobalHeader from './GlobalHeader'
+import ControlsContainer from './ControlsContainer'
+import { addStateManagement } from './StateManagement'
+import SnackbarContentWrapper from './SnackbarContentWrapper'
 export default class MainContainer extends React.Component {
-  
+
   constructor(props){
     super(props)
     let userProfile = UserProfile.getUserProfile()
     trackEvent(Constants.EVENT_CATEGORY_SEGMENT, "UserType", `${userProfile.userType}`, userProfile.numVisits)
-    
+
     let urlVariant = new URLSearchParams(window.location.search).get("variant")
-    let selectedVariant = urlVariant?urlVariant:Constants.VARIANT_STANDARD
+    let selectedVariant = urlVariant || Constants.VARIANT_STANDARD
     this.chess = chessLogic(selectedVariant)
     addStateManagement(this)
     this.state = {
@@ -64,7 +74,6 @@ export default class MainContainer extends React.Component {
     this.chessboardWidth = this.getChessboardWidth()
   }
 
-
   getMovesSettingsFromCookie(){
     let settings = cookieManager.getSettingsCookie()
     if(!settings) {
@@ -90,76 +99,86 @@ export default class MainContainer extends React.Component {
 
   render() {
     let lastMoveArray = this.state.lastMove ? [this.state.lastMove.from, this.state.lastMove.to] : null
-    let snackBarOpen = this.state.message?true:false
+    let snackBarOpen = Boolean(this.state.message)
+
     let playerMoves = this.getPlayerMoves()
     let bookMoves = this.getBookMoves()
     this.mergePlayerAndBookMoves(playerMoves, bookMoves)
-    return <div className="rootView"> 
-        <GlobalHeader toggleFeedback = {this.toggleFeedback(false)}/>
-        <Container className="mainContainer">
-          <Row><Col lg={{order:0, size:2}} xs={{order:2}}>
-            <Navigator fen = {this.state.fen} move={this.state.lastMove} 
-              onChange ={this.navigateTo.bind(this)}
-              variant = {this.state.variant}
-            />
-    </Col><Col lg="6"><Chessground key={this.state.resize}
-      height={this.chessboardWidth}
-      width={this.chessboardWidth}
-      orientation={this.orientation()}
-      turnColor={this.turnColor()}
-      movable={this.calcMovable()}
-      lastMove={lastMoveArray}
-      fen={this.state.fen}
-      onMove={this.onMoveAction.bind(this)}
-      drawable ={{
-        enabled: true,
-        visible: true,
-        autoShapes: this.autoShapes(playerMoves, this.state.highlightedMove)
-      }}
-      style={{ margin: 'auto' }}
-    />
-    </Col><Col lg="4" className="paddingTop"><ControlsContainer fen={this.state.fen}
-                resize ={this.state.resize} 
-                gamesProcessed={this.state.gamesProcessed} 
-                updateProcessedGames={this.updateProcessedGames.bind(this)}
-                settingsChange={this.settingsChange.bind(this)}
-                settings={this.state.settings}
-                reset={this.reset.bind(this)}
-                clear={this.clear.bind(this)}
-                playerMoves={playerMoves}
-                bookMoves={bookMoves}
-                gameResults={this.gameResults()}
-                onMove={this.onMove.bind(this)}
-                turnColor={this.turnColor()}
-                showError={this.showError.bind(this)}
-                showInfo={this.showInfo.bind(this)}
-                setDownloading={this.setDownloading.bind(this)}
-                isDownloading={this.state.downloadingGames}
-                openingGraph={this.state.openingGraph}
-                importCallback={this.importGameState.bind(this)}
-                variant={this.state.variant}
-                variantChange={this.variantChange.bind(this)}
-                forceFetchBookMoves={this.forceFetchBookMoves.bind(this)}
-                highlightArrow={this.highlightArrow.bind(this)}
-                /></Col>
-    </Row></Container>
-    <Snackbar anchorOrigin={{ vertical:'bottom', horizontal:"left" }} 
-            open={snackBarOpen} autoHideDuration={6000} 
-            onClose={this.closeError.bind(this)}
-    >
-    <SnackbarContentWrapper
-                            onClose={this.closeError.bind(this)}
-                            variant={this.state.messageSeverity}
-                            message={this.state.message}
-                            subMessage={this.state.subMessage}
-                            showReportButton={this.state.messageSeverity==='error'}
-                            action={this.state.errorAction}
-                            actionText={this.state.errorActionText}
-                        />
-    </Snackbar>
 
-    <Modal isOpen={this.state.feedbackOpen} toggle={this.toggleFeedback(false)}>
-        <ModalHeader toggle={this.toggleFeedback(false)}>Feedback</ModalHeader>
+    return <div className="rootView">
+      <GlobalHeader toggleFeedback = {this.toggleFeedback(false)}/>
+      <Container className="mainContainer">
+        <Row>
+          <Col lg={{order:0, size:2}} xs={{order:2}}>
+            <Navigator fen = {this.state.fen} move={this.state.lastMove}
+              onChange ={this.navigateTo.bind(this)}
+              variant = {this.state.variant} />
+          </Col>
+          <Col lg="6">
+            <Chessground key={this.state.resize}
+              height={this.chessboardWidth}
+              width={this.chessboardWidth}
+              orientation={this.orientation()}
+              turnColor={this.turnColor()}
+              movable={this.calcMovable()}
+              lastMove={lastMoveArray}
+              fen={this.state.fen}
+              onMove={this.onMoveAction.bind(this)}
+              drawable ={{
+                enabled: true,
+                visible: true,
+                autoShapes: this.autoShapes(playerMoves, this.state.highlightedMove)
+              }}
+              style={{ margin: 'auto' }}
+            />
+          </Col>
+          <Col lg="4" className="paddingTop">
+            <ControlsContainer fen={this.state.fen}
+              resize ={this.state.resize}
+              gamesProcessed={this.state.gamesProcessed}
+              updateProcessedGames={this.updateProcessedGames.bind(this)}
+              settingsChange={this.settingsChange.bind(this)}
+              settings={this.state.settings}
+              reset={this.reset.bind(this)}
+              clear={this.clear.bind(this)}
+              playerMoves={playerMoves}
+              bookMoves={bookMoves}
+              gameResults={this.gameResults()}
+              onMove={this.onMove.bind(this)}
+              turnColor={this.turnColor()}
+              showError={this.showError.bind(this)}
+              showInfo={this.showInfo.bind(this)}
+              setDownloading={this.setDownloading.bind(this)}
+              isDownloading={this.state.downloadingGames}
+              openingGraph={this.state.openingGraph}
+              importCallback={this.importGameState.bind(this)}
+              variant={this.state.variant}
+              variantChange={this.variantChange.bind(this)}
+              forceFetchBookMoves={this.forceFetchBookMoves.bind(this)}
+              highlightArrow={this.highlightArrow.bind(this)}
+            />
+          </Col>
+        </Row>
+      </Container>
+      <Snackbar anchorOrigin={{ vertical:'bottom', horizontal:"left" }}
+        open={snackBarOpen} autoHideDuration={6000}
+        onClose={this.closeError.bind(this)}
+      >
+        <SnackbarContentWrapper
+          onClose={this.closeError.bind(this)}
+          variant={this.state.messageSeverity}
+          message={this.state.message}
+          subMessage={this.state.subMessage}
+          showReportButton={this.state.messageSeverity==='error'}
+          action={this.state.errorAction}
+          actionText={this.state.errorActionText}
+        />
+      </Snackbar>
+
+      <Modal isOpen={this.state.feedbackOpen} toggle={this.toggleFeedback(false)}>
+        <ModalHeader toggle={this.toggleFeedback(false)}>
+          Feedback
+        </ModalHeader>
         <ModalBody>
           Your feedback is greatly appreciated. Reach out to me for feedback, suggestions, bug report or just a game of chess.
           <ul>
@@ -170,21 +189,20 @@ export default class MainContainer extends React.Component {
             <li>Join my <a rel="noopener noreferrer" href={Constants.OPENING_TREE_DISCORD}target="_blank">discord server</a> to chat</li>
           </ul>
           <FormControlLabel
-        control={
-          <Checkbox
-            checked={this.state.diagnosticsDataOpen}
-            onChange={this.toggleDiagnosticsData}
-            name="diagnostics"
-            color="primary"
+            control={
+              <Checkbox
+                checked={this.state.diagnosticsDataOpen}
+                onChange={this.toggleDiagnosticsData}
+                name="diagnostics"
+                color="primary"
+              />}
+            label="Add diagnostics data to message"
           />
-        }
-        label="Add diagnostics data to message"
-      />
           <Collapse isOpen={this.state.diagnosticsDataOpen}>
-            <TextField id="diagnosticsText" label="Click to copy." variant="outlined" 
-            className="fullWidth" value={this.getDiagnosticsValue()} 
-            rowsMax={4} onClick={this.copyDiagnostics} multiline/>
-            </Collapse>
+            <TextField id="diagnosticsText" label="Click to copy." variant="outlined"
+            className="fullWidth" value={this.getDiagnosticsValue()}
+            rowsMax={4} onClick={this.copyDiagnostics} multiline />
+          </Collapse>
         </ModalBody>
         <ModalFooter>
           <Button color="secondary" onClick={this.toggleFeedback(false)}>Done</Button>
